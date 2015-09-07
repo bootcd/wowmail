4e69eabe5a8cab6e688e8e6413b4c4a63a99e55d
mihail <mihail@192.168.1.2>
2015-09-07 21:21:27 +0300

    New Class "Attachment" added

---
+++ src/mailer/Attachment.java
@@ -0,0 +1,24 @@
+package mailer;
+
+import java.util.Scanner;
+
+public class Attachment  {
+
+	
+	String fileName;
+	String pathName;
+	
+	
+	public void setPath(String pathName){
+	
+		this.pathName=pathName;
+	}
+	
+	
+	public String getPathName(){
+		return this.pathName;
+	}
+	
+
+
+}
